1c09830156242d0e29ef772db7d36643edc974d8
Yannick Patois <yannick.patois@iphc.cnrs.fr>
2016-05-26 11:05:03 +0200

    A start

---
+++ notifreesms.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import requests
+import appdirs
+
+# https://smsapi.free-mobile.fr/sendmsg?user=<>&pass=<>&msg=<>
+
+class Contacts:
+  def __init__(self):
+    pass
+
+
+# --------------------------------------------------------------------------
+# Welcome to Derry, Maine
+# --------------------------------------------------------------------------
+def main():
+  pass
+
+
+
+if __name__ == '__main__':
+  main()
+
