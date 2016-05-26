33aac03a687fafb94b898f93096e0621ade18450
Yannick Patois <yannick.patois@iphc.cnrs.fr>
2016-05-26 11:25:39 +0200

    First adding contacts management

--- notifreesms.py
+++ notifreesms.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
+import pickle
+import argparse
+
 import requests
 import appdirs
 
@@ -8,7 +12,20 @@ import appdirs
 
 class Contacts:
   def __init__(self):
-    pass
+    _appname="NBotiFreeSms"
+    _appauthor = "YP"
+    _config_dir=user_config_dir(_appname,_appauthor)
+    if (not os.direxists(_config_dir)):
+      os.makedirs(_config_dir)
+    self._file_name=os.path.join(_config_dir,"contacts.dat")
+    if (os.path.isfile(self._file_name)):
+      f=open(self._file_name)
+        self._contacts=pickle.load(f)
+    else:
+      self._contacts={}
+     
+    
+    
 
 
 # --------------------------------------------------------------------------
