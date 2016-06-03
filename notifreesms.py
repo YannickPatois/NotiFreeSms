497bcc860e6903ae8df56d32168a785c02763e27
Yannick Patois <yannick.patois@iphc.cnrs.fr>
2016-06-03 10:07:56 +0200

    Start to do something

--- notifreesms.py
+++ notifreesms.py
@@ -10,12 +10,23 @@ import appdirs
 
 # https://smsapi.free-mobile.fr/sendmsg?user=<>&pass=<>&msg=<>
 
+class Contact:
+  def __init__(self,name,cid,cpass):
+    self._name=name
+    self._id=cid
+    self._pass=cpass
+
+  def __str__(self):
+    return self._name+" "+self._id+" "+self._pass
+  
+
+
 class Contacts:
   def __init__(self):
-    _appname="NBotiFreeSms"
+    _appname="NotiFreeSms"
     _appauthor = "YP"
-    _config_dir=user_config_dir(_appname,_appauthor)
-    if (not os.direxists(_config_dir)):
+    _config_dir=appdirs.user_config_dir(_appname,_appauthor)
+    if (not os.path.isdir(_config_dir)):
       os.makedirs(_config_dir)
     self._file_name=os.path.join(_config_dir,"contacts.dat")
     if (os.path.isfile(self._file_name)):
@@ -24,7 +35,22 @@ class Contacts:
     else:
       self._contacts={}
      
-    
+  def __str__(self):
+    s="Contacts: "
+    for c in self._contacts:
+      s+="\n"+str(c)
+    s+="\n"
+    return s
+
+
+def add(c):
+    if (self._contacts.has_key(c._name)):
+      print ("Erreur: un contact avec le même nom ("+c._name+") existe déjà")
+      print ("Utilisez --replacecontact et non --addcontact")
+      return
+    self._contacts[c._name]=c
+ 
+  
     
 
 
@@ -35,7 +61,7 @@ def main():
   parser = argparse.ArgumentParser(description="""
     Permet d'utiliser l'option Notification SMS Free Mobile SMS depuis un PC
     """)
-  parser.add_argument('--newcontact', dest='contact', action='store_true',
+  parser.add_argument('--addcontact', dest='addcontact', action='store_true',
                       default=False,
                       help="Nouveau contact")
   parser.add_argument('--name', dest='name', action='store',
@@ -49,7 +75,16 @@ def main():
                       help="Mot de passe Free du contact")
   
   args = parser.parse_args()
-
+  
+  ct=Contacts()
+  
+  if (args.addcontact):
+    print ("Ajout d'un contact")
+    c=Contact(args.name,args.userid,args.userpass)
+    print c
+    ct.add(c)
+    print ct
+    ct.write()
 
 
 if __name__ == '__main__':
